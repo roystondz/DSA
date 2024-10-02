fc704300e1b6b7b23de01567c18fad696c6a2116
Royston Akash Dsouza <116806132+roystondz@users.noreply.github.com>
2024-10-02 11:24:19 +0530

    Update 11ContainerWater.cpp

--- 11ContainerWater.cpp
+++ 11ContainerWater.cpp
@@ -16,3 +16,20 @@ public:
     }
 };
 
+#maxContainer with optimal approach
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxContainer=0,i=0;
+        int j = height.size()-1;
+        while(j>i){
+            int w=j-i;
+            int h=min(height[i],height[j]);
+            int area=w*h;
+            maxContainer=max(area,maxContainer);
+            height[i]<height[j]?i++:j--;
+        }
+        return maxContainer;
+    }
+};
+
